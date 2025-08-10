4d06c27e117459515df12c88036b156bec2345a9
Reville <yurireville2901@gmail.com>
2025-08-11 01:35:31 +0800

    Add Laboratory Exercise 1 & 2 by Reville
    SUMMARY
    
    By Watching some Youtube tutorial and reading the handouts i Learned how to make a simple program that take the user input and display them, and also i learned how to use If else statement.
    the code that imade is about Arknights game, you will register your Code name, Faction, and your Combat Experience after entering all of those input your information will me shown to you and you will be asked if you want to deploy on a mission or not.

---
+++ TestRun/Student/Reville.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rhodes_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[--- Arknights Operator Registration ---]");
+
+            // Ask for operator details
+            Console.Write("Enter Operator Code Name: ");
+            string codeName = Console.ReadLine();
+
+            Console.Write("Enter Faction Name: ");
+            string faction = Console.ReadLine();
+
+            Console.Write("Enter Combat Experience:");
+            string combatExp = Console.ReadLine();
+
+            // Display entered info
+            Console.WriteLine("\n--- Operator Profile ---");
+            Console.WriteLine("Code Name: " + codeName);
+            Console.WriteLine("Faction: " + faction);
+            Console.WriteLine("Combat Experience: " + combatExp);
+
+            // Deployment decision
+            Console.Write("\nDo you want to deploy on a mission? [Yes/No]: ");
+            string response = Console.ReadLine();
+
+            if (response.ToLower() == "yes")
+            {
+                Console.WriteLine("Deployment successful! " + codeName + " is on the mission.");
+            }
+            else
+            {
+                Console.WriteLine("Waiting for next Operator.");
+            }
+
+            Console.WriteLine("\n--- Author by : Reville ---");
+        }
+    }
+}
