70b77b5de69dec961cc9f977e06cec3a3a61f5c9
Tenerife-Richel <teneriferichel01@gmail.com>
2025-08-11 22:38:23 +0800

    Add files via upload

---
+++ TestRun/Student/Tenerife.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LabActivity
+{
+    public class Tenerife
+    {
+        public void Run()
+        {
+            
+            int age = 19;
+            float gwa = 1.34f;
+            string name = "Richel Tenerife";
+            bool isEnrolled = true;
+
+            
+            Console.WriteLine("=== Student Info ===");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("GWA: " + gwa);
+            Console.WriteLine("Enrolled: " + isEnrolled);
+
+            
+            if (isEnrolled)
+            {
+                Console.WriteLine(name + " is currently enrolled.");
+            }
+            else
+            {
+                Console.WriteLine(name + " is not enrolled.");
+            }
+
+           
+            Console.WriteLine("Author by: Tenerife");
+        }
+    }
+}
