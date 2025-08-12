c2c67955db0c4c079c090cab3cb2bf5c599d8fc8
Kharl879 <kharlsten69@gmail.com>
2025-08-12 08:46:13 +0800

    Add files via upload

--- TestRun/Student/Pardillo.cs
+++ TestRun/Student/Pardillo.cs
@@ -1,24 +1,38 @@
-﻿
-string fruit = "Mango";
-int quantity = 5;
-float pricePerPiece = 12.5f;
-bool isRipe = true;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-Console.WriteLine("Fruit: " + fruit);
-Console.WriteLine("Quantity: " + quantity);
-Console.WriteLine("Price per piece: " + pricePerPiece + " Pesos");
-
-if (isRipe)
+namespace TestRun.Student
 {
-    Console.WriteLine("The fruit is ripe");
+    internal class Pardillo : Student
+    {
+        public override void Run()
+        {
+
+            string fruit = "Mango";
+            int quantity = 5;
+            float pricePerPiece = 12.5f;
+            bool isRipe = true;
+
+            Console.WriteLine("Fruit: " + fruit);
+            Console.WriteLine("Quantity: " + quantity);
+            Console.WriteLine("Price per piece: " + pricePerPiece + " Pesos");
+
+            if (isRipe)
+            {
+                Console.WriteLine("The fruit is ripe");
+            }
+            else
+            {
+                Console.WriteLine("The fruit is not ripe yet");
+            }
+
+            Console.WriteLine("\nAuthor by: Pardillo");
+
+        }
+   
+    }
 }
-else
-{
-    Console.WriteLine("The fruit is not ripe yet");
-}
-
-Console.WriteLine("\nAuthor by: Pardillo");
-
-
-
 
